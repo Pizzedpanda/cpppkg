75d755e7165b4411c135cf689113d98de0a76676
Pizzedpanda <90967717+Pizzedpanda@users.noreply.github.com>
2024-09-04 13:15:47 +1000

    post_init.cmake

--- post_init.cmake
+++ post_init.cmake
@@ -1,14 +1,14 @@
-add_library(pico_extras_included INTERFACE)
-target_compile_definitions(pico_extras_included INTERFACE
-        -DPICO_EXTRAS=1
+add_library(pico_cpppkg_included INTERFACE)
+target_compile_definitions(pico_cpppkg_included INTERFACE
+        -DPICO_cpppkg=1
         )
 
-pico_add_platform_library(pico_extras_included)
+pico_add_platform_library(pico_cpppkg_included)
 
 # note as we're a .cmake included by the SDK, we're relative to the pico-sdk build
-add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/src ${CMAKE_BINARY_DIR}/pico_extras/src)
+add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/src ${CMAKE_BINARY_DIR}/pico_cpppkg/src)
 
-if (PICO_EXTRAS_TESTS_ENABLED OR PICO_EXTRAS_TOP_LEVEL_PROJECT)
-    add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/test ${CMAKE_BINARY_DIR}/pico_extras/test)
+if (PICO_cpppkg_TESTS_ENABLED OR PICO_cpppkg_TOP_LEVEL_PROJECT)
+    add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/test ${CMAKE_BINARY_DIR}/pico_cpppkg/test)
 endif ()
 
